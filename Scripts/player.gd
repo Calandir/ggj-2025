f00ffdff888af0f45c1963079887fab7a4cd28dd
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-01-25 12:27:10 +1300

    Add option to use separate joystick for rotation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,30 @@
 extends CharacterBody2D
 
+const TURN_WITH_SEPARATE_CONTROLS: bool = true
+
 const SPEED = 300.0
 
 @export var player_num: int = 0
 
-func apply_input() -> Vector2:
+func _physics_process(delta):
+	var inputVector2 = _get_move_input()
+	
+	velocity = inputVector2
+	
+	if TURN_WITH_SEPARATE_CONTROLS:
+		_process_rotation(_get_look_input())
+	else:
+		_process_rotation(inputVector2)
+		
+	move_and_slide()
+
+func _process_rotation(input_direction: Vector2):
+	if not input_direction.is_zero_approx():
+		rotation_degrees = 90.0 + rad_to_deg(input_direction.angle())
+		
+		#print(str(input_direction) + " " + str(input_direction.angle()))
+
+func _get_move_input() -> Vector2:
 	var input_prefix = "p" + str(player_num)
 	
 	var input_direction = Input.get_vector(
@@ -15,14 +35,13 @@ func apply_input() -> Vector2:
 	
 	return input_direction * SPEED
 
-func _physics_process(delta):
-	var inputVector2 = apply_input()
+func _get_look_input() -> Vector2:
+	var input_prefix = "p" + str(player_num)
 	
-	velocity = inputVector2
+	var input_direction = Input.get_vector(
+		input_prefix + "_look_left",
+		input_prefix + "_look_right",
+		input_prefix + "_look_up",
+		input_prefix + "_look_down")
 	
-	if not velocity.is_zero_approx():
-		rotation_degrees = 90.0 + rad_to_deg(inputVector2.angle())
-		
-		#print(str(inputVector2) + " " + str(inputVector2.angle()))
-		
-	move_and_slide()
+	return input_direction * SPEED
