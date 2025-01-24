172ea9d7648bf67d16d2edd2e16930d6fc1c41fe
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-01-25 12:10:34 +1300

    Face the way you're moving

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,7 +4,7 @@ const SPEED = 300.0
 
 @export var player_num: int = 0
 
-func apply_input():
+func apply_input() -> Vector2:
 	var input_prefix = "p" + str(player_num)
 	
 	var input_direction = Input.get_vector(
@@ -13,8 +13,16 @@ func apply_input():
 		input_prefix + "_up",
 		input_prefix + "_down")
 	
-	velocity = input_direction * SPEED
+	return input_direction * SPEED
 
 func _physics_process(delta):
-	apply_input()
+	var inputVector2 = apply_input()
+	
+	velocity = inputVector2
+	
+	if not velocity.is_zero_approx():
+		rotation_degrees = 90.0 + rad_to_deg(inputVector2.angle())
+		
+		#print(str(inputVector2) + " " + str(inputVector2.angle()))
+		
 	move_and_slide()
