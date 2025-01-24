adcdfa797821f1ba63e13fdfea77ebb3041b8df0
DiscotekNZ <kagenesbit@gmail.com>
2025-01-25 12:55:30 +1300

    Added goal and start of UI

---
+++ Scenes/goal.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+# Keep track of bubbles eaten by goal
+
+var score = 0
+
+@onready var score_label = get_node("Label")  # Adjust path to your ScoreManager node
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+		
+		
+		
+	
+
+signal goal_scored
+
+func _on_Area2D_body_entered(body):
+	#print("collision detected!")
+	if body is RigidBody2D:
+		score += 1
+		body.queue_free()
+		#print("score!")	
+		emit_signal("goal_scored")
+	
+	
+
+#func _on_Area2D_body_exited(body):
+	#if body is RigidBody2D:
+		#bubbles_in_goal.erase(body)
---
+++ Scenes/label.gd
@@ -0,0 +1,31 @@
+extends Label
+
+# Declare the score variable
+var score : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.text = "Score: " + str(score)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+# Reference to the Label node
+@onready var score_label = $Label
+
+# Function to update the score and update the UI text
+func update_score():
+	score += 1
+	self.text = "Score: " + str(score)
+
+
+func _on_goal_goal_scored() -> void:
+	score += 1
+	self.text = "Score: " + str(score)
