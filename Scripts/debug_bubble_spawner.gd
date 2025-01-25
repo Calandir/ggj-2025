5339004aea32a379a7e94b56d08b2d24210c949c
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-01-26 11:08:55 +1300

    Add manual bubble spawn option

--- Scripts/debug_bubble_spawner.gd
+++ Scripts/debug_bubble_spawner.gd
@@ -1,14 +1,18 @@
 extends Node
 
-const USE_AUTO_SPAWN: bool = true
+const USE_AUTO_SPAWN: bool = false
+const USE_MANUAL_SPAWN: bool = true
 
 const TIME_BETWEEN_BUBBLES: float = 3.0
 var _time_until_next_bubble: float = 0.0
 
-@onready var bubble_scene = preload("res://Scenes/Bubble.tscn")
+@export var _p0_spawn_topleft: Node2D
+@export var _p0_spawn_bottomRight: Node2D
 
-func _ready():
-	_time_until_next_bubble = TIME_BETWEEN_BUBBLES
+@export var _p1_spawn_topleft: Node2D
+@export var _p1_spawn_bottomRight: Node2D
+
+@onready var bubble_scene = preload("res://Scenes/Bubble.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,6 +21,8 @@ func _process(delta):
 	
 	if USE_AUTO_SPAWN:
 		_process_auto_spawn(delta)
+	if USE_MANUAL_SPAWN:
+		_process_manual_spawn()
 
 func _process_auto_spawn(delta):
 	_time_until_next_bubble -= delta
@@ -26,3 +32,18 @@ func _process_auto_spawn(delta):
 	
 	_time_until_next_bubble += TIME_BETWEEN_BUBBLES
 	get_tree().root.add_child(bubble_scene.instantiate())
+
+func _process_manual_spawn():
+	if Input.is_action_just_pressed("p0_spawn_bubble"):
+		_spawn_bubble_on_side(_p0_spawn_topleft, _p0_spawn_bottomRight)
+	if Input.is_action_just_pressed("p1_spawn_bubble"):
+		_spawn_bubble_on_side(_p1_spawn_topleft, _p1_spawn_bottomRight)
+
+func _spawn_bubble_on_side(topleft_corner: Node2D, bottomright_corner: Node2D):
+	var spawn_point = Vector2(randf_range(topleft_corner.position.x, bottomright_corner.position.x), randf_range(topleft_corner.position.y, bottomright_corner.position.y))
+	
+	var new_bubble: Node2D = bubble_scene.instantiate()
+	new_bubble.position = spawn_point
+	print(spawn_point)
+	
+	get_tree().root.add_child(new_bubble)
