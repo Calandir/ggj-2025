10047502c3e102cbe9daedb9d069f77c528da419
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-01-26 11:25:16 +1300

    Auto spawn bubbles has random range

--- Scripts/bubble_spawner.gd
+++ Scripts/bubble_spawner.gd
@@ -1,10 +1,13 @@
 extends Node
 
-const USE_AUTO_SPAWN: bool = false
-const USE_MANUAL_SPAWN: bool = true
+const USE_AUTO_SPAWN: bool = true
+const USE_MANUAL_SPAWN: bool = false
 
 const TIME_BETWEEN_BUBBLES: float = 3.0
 var _time_until_next_bubble: float = 0.0
+
+@export var _auto_spawn_topleft: Node2D
+@export var _auto_spawn_bottomRight: Node2D
 
 @export var _p0_spawn_topleft: Node2D
 @export var _p0_spawn_bottomRight: Node2D
@@ -31,15 +34,15 @@ func _process_auto_spawn(delta):
 		return
 	
 	_time_until_next_bubble += TIME_BETWEEN_BUBBLES
-	get_tree().root.add_child(bubble_scene.instantiate())
+	_spawn_bubble_in_area(_auto_spawn_topleft, _auto_spawn_bottomRight)
 
 func _process_manual_spawn():
 	if Input.is_action_just_pressed("p0_spawn_bubble"):
-		_spawn_bubble_on_side(_p0_spawn_topleft, _p0_spawn_bottomRight)
+		_spawn_bubble_in_area(_p0_spawn_topleft, _p0_spawn_bottomRight)
 	if Input.is_action_just_pressed("p1_spawn_bubble"):
-		_spawn_bubble_on_side(_p1_spawn_topleft, _p1_spawn_bottomRight)
+		_spawn_bubble_in_area(_p1_spawn_topleft, _p1_spawn_bottomRight)
 
-func _spawn_bubble_on_side(topleft_corner: Node2D, bottomright_corner: Node2D):
+func _spawn_bubble_in_area(topleft_corner: Node2D, bottomright_corner: Node2D):
 	var spawn_point = Vector2(randf_range(topleft_corner.position.x, bottomright_corner.position.x), randf_range(topleft_corner.position.y, bottomright_corner.position.y))
 	
 	var new_bubble: Node2D = bubble_scene.instantiate()
