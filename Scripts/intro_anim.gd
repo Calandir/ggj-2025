84dab6963f9fcd071f3719d5e064b36ec7471b2a
Christopher Brown <christopher.chirat.brown@gmail.com>
2025-01-26 15:27:38 +1300

    Edit bubble intro anim easing, don't know if it's noticeable

--- Scripts/intro_anim.gd
+++ Scripts/intro_anim.gd
@@ -8,8 +8,11 @@ func _ready():
 	parent.scale = 1.4 * Vector2.ONE
 	parent.modulate = Color.TRANSPARENT
 	
+	# Using 2 separate tweens in case they need different settings
 	var tween_1 = get_tree().create_tween()
+	tween_1.set_ease(Tween.EASE_OUT)
 	tween_1.tween_property(parent, "scale", Vector2.ONE, 0.5)
 	
 	var tween_2 = get_tree().create_tween()
+	tween_2.set_ease(Tween.EASE_OUT)
 	tween_2.tween_property(parent, "modulate", Color.WHITE, 0.3)
